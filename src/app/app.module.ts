3195b3ddb05365832b49eb2f8c9d3d46c118b74d
Aneesh Kallarakkal <kallarakkal@hotmail.com>
2019-04-12 11:04:41 +0200

    Add stub components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { ProductSearchComponent } from './components/product-search/product-search.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductSearchComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
